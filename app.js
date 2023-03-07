acc159e2cb19a32074f9b7104619bf5a292d1958
Ben Halliwell <bkhalliwell@outlook.com>
2023-03-07 19:48:07 +0000

    modified:   app.js

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 'use strict'; 
 
-let wantsASunday = prompt('Can we make you a sunday?');
+let wantsASunday = prompt('Can we make you a sunday?').toLowerCase();
+
 let toppings = ['Hot Fudge'; 'Cherries'; 'Sprinkles'; 'Bubblegum'];
 
 if (wantsASunday == 'yes') {
     alert('Awesome! We have lots of variety!');
-    let iceCreamFlavor = prompt('What kind of icecream would you like? We have chocolate, vanilla, and coffee.')
+    let iceCreamFlavor = prompt('What kind of icecream would you like? We have chocolate, vanilla, and coffee.').toLowerCase();
     
     if(iceCreamFlavor === 'chocolate' || iceCreamFlavor === 'vanilla' || iceCreamFlavor === 'cofee') {
         alert('Perfect! Please choose up to three options from our list of toppings. We have Fudge, Cherries, Spinkles, and Bubblegum');
-        let toppingOne = prompt('What would you like for your first topping?');
+        let toppingOne = prompt('What would you like for your first topping?').toLowerCase();
         alert(toppingOne + ', great! What else?');
-        let toppingTwo = prompt('What would you like for your second topping, in addition to ${toppingOne}?');
+        let toppingTwo = prompt('What would you like for your second topping, in addition to ${toppingOne}?').toLowerCase();
     
     } else {
         alert('Unfortunately we don\'t have that flavor.');
