43d20eba5ba983f2e13c22d6350d630ce138df7c
Ben Halliwell <bkhalliwell@outlook.com>
2023-03-07 19:41:59 +0000

    new file:   app.js 	new file:   index.html

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict'; 
+
+let wantsASunday = prompt('Can we make you a sunday?');
+let toppings = ['Hot Fudge'; 'Cherries'; 'Sprinkles'; 'Bubblegum'];
+
+if (wantsASunday == 'yes') {
+    alert('Awesome! We have lots of variety!');
+    let iceCreamFlavor = prompt('What kind of icecream would you like? We have chocolate, vanilla, and coffee.')
+    
+    if(iceCreamFlavor === 'chocolate' || iceCreamFlavor === 'vanilla' || iceCreamFlavor === 'cofee') {
+        alert('Perfect! Please choose up to three options from our list of toppings. We have Fudge, Cherries, Spinkles, and Bubblegum');
+        let toppingOne = prompt('What would you like for your first topping?');
+        alert(toppingOne + ', great! What else?');
+        let toppingTwo = prompt('What would you like for your second topping, in addition to ${toppingOne}?');
+    
+    } else {
+        alert('Unfortunately we don\'t have that flavor.');
+    }
+
+} else {
+    alert('OK! Let us know if you change your mind!');
+}
